e5a0afe0c5ae8ec4626b1ff70a30b7ae2d6b29d9
Enbiya Göral <100806254+enbiyagoral@users.noreply.github.com>
2023-09-23 13:23:47 +0300

    Course Done!

---
+++ src/items/items.controller.ts
@@ -0,0 +1,38 @@
+import { Controller, Get , Post, Put , Delete, Body, Param} from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item-dto';
+import { UpdateItemDto } from './dto/update-item-dto';
+import { ItemsService } from './items.service';
+import { Item } from './interfaces/items.interface';
+
+
+@Controller('items')
+export class ItemsController {
+    constructor(private readonly itemsService: ItemsService){}
+
+    @Get()
+    findAll(): Promise<Item[]>{
+        return this.itemsService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id:string): Promise<Item>{
+        return this.itemsService.findOne(id)
+
+    }
+
+    @Post()
+    create(@Body() createItemDto: CreateItemDto): Promise<Item>{
+        return this.itemsService.create(createItemDto);
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id:string) : Promise<Item>{
+        return this.itemsService.delete(id);
+    }
+
+    @Put(':id')
+    update(@Body() updateItemDto: UpdateItemDto, @Param('id') id:string): Promise<Item>{
+        return this.itemsService.update(id,updateItemDto);
+
+    }
+}
